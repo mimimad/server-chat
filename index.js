e801fea627dded4f2bd6db4ee63313e319a446f6
Mikhail Zakharov <Zakharov3010@yandex.ru>
2021-04-09 10:48:22 +0300

    add users methods

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const socketio = require('socket.io');
 const cors = require('cors');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+
+const router = require('./router');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
