3c63cbd5bf926e2975d67e5cbefc3d5e843e4824
Mikhail Zakharov <Zakharov3010@yandex.ru>
2021-04-11 20:57:30 +0300

    add sockets and comments

--- index.js
+++ index.js
@@ -9,23 +9,47 @@ const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, {cors: {origin: "*"}});
 
+// указываем порт
 const { PORT = 8000 } = process.env;
 
+// мидлвары
 app.use(cors());
 app.use(router);
 
+// сокеты
+io.on('connect', (socket) => {
 
+    //подключение юзера
+    socket.on('join', ({ name, room }) => {
 
+        const { user } = addUser({ id: socket.id, name, room });
 
+        socket.join(user.room);
+// рассылка сообщений от admin
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`, time: new Date().toLocaleTimeString()});
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!`, time: new Date().toLocaleTimeString() });
 
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
 
+    });
+// отправка сообщения
+    socket.on('sendMessage', (message) => {
+        const user = getUser(socket.id);
+        io.to(user.room).emit('message', { user: user.name, text: message, time: new Date().toLocaleTimeString() });
 
+    });
+// отключение юзера
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id);
 
+        if(user) {
+            io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left.`, time: new Date().toLocaleTimeString() });
+            io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+        }
+    })
+});
 
-
-
-
-
+// запуск сервера
 server.listen(PORT, () => console.log(`Express listened on port ${PORT}`));
--- users.js
+++ users.js
@@ -1,5 +1,10 @@
+// массив юзеров
 const users = [];
 
+//доп методы для работы с массивом
+
+
+// функция добавления юзера
 const addUser = ({ id, name, room }) => {
     name = name.trim().toLowerCase();
     room = room.trim().toLowerCase();
@@ -16,14 +21,17 @@ const addUser = ({ id, name, room }) => {
     return { user };
 }
 
+// функция удаления юзера
 const removeUser = (id) => {
     const index = users.findIndex((user) => user.id === id);
 
     if(index !== -1) return users.splice(index, 1)[0];
 }
 
+// поиск юзера по id
 const getUser = (id) => users.find((user) => user.id === id);
 
+// возвращаем всех юзеров в комнате
 const getUsersInRoom = (room) => users.filter((user) => user.room === room);
 
 module.exports = { addUser, removeUser, getUser, getUsersInRoom };
